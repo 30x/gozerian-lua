771a43e6efc1d9b2a985c556716382bcdf92e79a
Scott Ganyo <scott@ganyo.com>
2016-06-29 17:35:01 -0700

    ability to create handlers in nginx config

--- lib/resty/gozerian/lua-gozerian.lua
+++ lib/resty/gozerian/lua-gozerian.lua
@@ -1,6 +1,17 @@
 local gozerian = {}
-function gozerian.init()
+function gozerian.init(handlers)
   gobridge = require('gozerian-init')
+
+  for key,value in pairs(handlers) do
+    local cfgErr = gobridge.GoCreateHandler(key, value)
+    if not (cfgErr == nil) then
+      local errMsg = ffi.string(cfgErr)
+      print('Error loading configuration: ' .. errMsg)
+      ffi.C.free(cfgErr)
+      return
+    end
+  end
+
   return gobridge
 end
 
