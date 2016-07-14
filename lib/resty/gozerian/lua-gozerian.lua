bf42335b283b15286557441a1df785ee767758d8
Scott Ganyo <scott@ganyo.com>
2016-07-14 14:23:32 -0700

    cleanup

--- lib/resty/gozerian/lua-gozerian.lua
+++ lib/resty/gozerian/lua-gozerian.lua
@@ -14,16 +14,4 @@ function gozerian.init(handlers)
 
   return gobridge
 end
-
-function gozerian.body_filter()
-  require('gozerian-body-filter')
-end
-
-function gozerian.process_request()
-  require('gozerian-request')
-end
-function gozerian.header_filter()
-  require('gozerian-header-filter')
-end
-
 return gozerian
